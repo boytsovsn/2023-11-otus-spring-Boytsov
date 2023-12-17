b6d3af3973374347a31c872b0841c4e82837e28f
boytsovsn <caracupa@inbox.ru>
2023-12-17 17:47:41 +0700

    Расставлены стереотипы. Приложение запустилось.

--- hw2/src/main/java/ru/otus/hw/config/AppProperties.java
+++ hw2/src/main/java/ru/otus/hw/config/AppProperties.java
@@ -2,14 +2,20 @@ package ru.otus.hw.config;
 
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Setter
+@Configuration
+@PropertySource("classpath:application.properties")
 public class AppProperties implements TestConfig, TestFileNameProvider {
 
     // внедрить свойство из application.properties
+    @Value("${test.rightAnswersCountToPass}")
     private int rightAnswersCountToPass;
 
     // внедрить свойство из application.properties
+    @Value("${test.fileName}")
     private String testFileName;
 
     @Override
