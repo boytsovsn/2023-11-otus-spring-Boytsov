974d5e09cc00e95b38a03962c9b448ce0676c303
boytsovsn <caracupa@inbox.ru>
2023-12-26 01:47:21 +0700

    Приложение сгенерированное на httpsstart.spring.io с такими зависимостями: Spring Shell I/O Build command line applications with spring. Lombok DEVELOPER TOOLS Java annotation library which helps to reduce boilerplate code. Spring Configuration Processor DEVELOPER TOOLS Generate metadata for developers to offer contextual help and "code completion" when working with custom configuration keys (ex.application.properties/.yml files).

---
+++ hw03-httpsstart-spring-io/src/test/java/ru/otus/hw/hw03springboot/Hw03SpringBootApplicationTests.java
@@ -0,0 +1,16 @@
+package ru.otus.hw.hw03springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class Hw03SpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+		assertEquals(1, 0);
+	}
+
+}
