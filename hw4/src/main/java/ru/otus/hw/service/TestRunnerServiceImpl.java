fd2d7dc4bb79af645538f67cf3b95da86e284316
boytsovsn <caracupa@inbox.ru>
2023-12-31 00:33:14 +0700

    Основной функционал работает

--- hw4/src/main/java/ru/otus/hw/service/TestRunnerServiceImpl.java
+++ hw4/src/main/java/ru/otus/hw/service/TestRunnerServiceImpl.java
@@ -2,10 +2,18 @@ package ru.otus.hw.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.shell.Availability;
+import org.springframework.shell.standard.ShellComponent;
+import org.springframework.shell.standard.ShellMethod;
+import org.springframework.shell.standard.ShellMethodAvailability;
+import org.springframework.shell.standard.ShellOption;
 import org.springframework.stereotype.Service;
+import ru.otus.hw.domain.Student;
+import ru.otus.hw.domain.TestResult;
 
 @Service
 @RequiredArgsConstructor
+@ShellComponent
 public class TestRunnerServiceImpl implements TestRunnerService {
 
     private final TestService testService;
@@ -14,15 +22,45 @@ public class TestRunnerServiceImpl implements TestRunnerService {
 
     private final ResultService resultService;
 
+    private Student student;
+
+    private TestResult testResult;
+
     @Override
     public void run() {
-        var student = studentService.determineCurrentStudent();
-        var testResult = testService.executeTestFor(student);
+
+    }
+
+    @ShellMethod(value = "Login command", key = {"l", "login"})
+    public String login(@ShellOption(defaultValue = "AnyUser") String userName) {
+        student = studentService.determineCurrentStudent();
+        return "Вы зарегистрировались. Пройти тест - команда (test).";
+    }
+
+    @ShellMethod(value = "Test command", key = {"t", "test"})
+    @ShellMethodAvailability(value = "isTestCommandAvailable")
+    public String test() {
+        testResult = testService.executeTestFor(student);
+        return "Посмотреть результат теста (команда show).";
+    }
+
+    @ShellMethod(value = "Show result command", key = {"s", "show"})
+    @ShellMethodAvailability(value = "isShowCommandAvailable")
+    public String show() {
         resultService.showResult(testResult);
+        return "Выйти - команда exit.";
+    }
+
+    private Availability isShowCommandAvailable() {
+        return testResult == null? Availability.unavailable("Пройдите тест (команда тест)."): Availability.available();
+    }
+
+    private Availability isTestCommandAvailable() {
+        return student == null? Availability.unavailable("Сначала зарегистрируйтесь (команда login)."): Availability.available();
     }
 
     @Override
     public void run(String... args) throws Exception {
-        run();
     }
+
 }
