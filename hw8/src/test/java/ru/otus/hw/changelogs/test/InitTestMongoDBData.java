52121831b8887f7f0833c358f390233bb4b97a24
boytsovsn <caracupa@inbox.ru>
2024-02-02 07:03:35 +0700

    тестовая бд

--- hw8/src/test/java/ru/otus/hw/changelogs/test/InitTestMongoDBData.java
+++ hw8/src/test/java/ru/otus/hw/changelogs/test/InitTestMongoDBData.java
@@ -34,36 +34,36 @@ public class InitTestMongoDBData {
     @ChangeSet(order = "001", id = "initAuthor", author = "boytsov", runAlways = true)
     public void initAuthor(AuthorRepository repository) {
         authors = new ArrayList<>();
-        authors.add(repository.save(new Author("Конан Дойл")));
-        authors.add(repository.save(new Author("Агата Кристи")));
-        authors.add(repository.save(new Author("Александр Пушкин")));
+        authors.add(repository.save(new Author("Author_1")));
+        authors.add(repository.save(new Author("Author_2")));
+        authors.add(repository.save(new Author("Author_3")));
     }
 
     @ChangeSet(order = "002", id = "initGenre", author = "boytsov", runAlways = true)
     public void initGenre(GenreRepository repository) {
         genres = new ArrayList<>();
-        genres.add(repository.save(new Genre("Детектив")));
-        genres.add(repository.save(new Genre("Роман")));
-        genres.add(repository.save(new Genre("Поэма")));
+        genres.add(repository.save(new Genre("Genre_1")));
+        genres.add(repository.save(new Genre("Genre_2")));
+        genres.add(repository.save(new Genre("Genre_3")));
     }
 
     @ChangeSet(order = "003", id = "initBooks", author = "boytsov", runAlways = true)
     public void initBooks(BookRepository repository) {
         books = new ArrayList<>();
-        books.add(repository.save(new Book("Шерлок Холмс", authors.get(0), genres.get(0))));
-        books.add(repository.save(new Book("9 негритят", authors.get(1), genres.get(0))));
-        books.add(repository.save(new Book("Капитанская дочка", authors.get(2), genres.get(1))));
+        books.add(repository.save(new Book("BookTitle_1", authors.get(0), genres.get(0))));
+        books.add(repository.save(new Book("BookTitle_2", authors.get(1), genres.get(1))));
+        books.add(repository.save(new Book("BookTitle_3", authors.get(2), genres.get(2))));
     }
 
     @ChangeSet(order = "004", id = "initRemarks", author = "boytsov", runAlways = true)
     public void initRemarks(RemarkRepository repository) {
         remarks = new ArrayList<>();
-        remarks.add(repository.save(new Remark("Круто!!!", books.get(0).getId())));
-        remarks.add(repository.save(new Remark("Не очень.", books.get(0).getId())));
-        remarks.add(repository.save(new Remark("Скучно.", books.get(1).getId())));
-        remarks.add(repository.save(new Remark("Интересно", books.get(2).getId())));
-        remarks.add(repository.save(new Remark("Классика", books.get(2).getId())));
-        remarks.add(repository.save(new Remark("Долго", books.get(2).getId())));
+        remarks.add(repository.save(new Remark("Remark_11", books.get(0).getId())));
+        remarks.add(repository.save(new Remark("Remark_21", books.get(1).getId())));
+        remarks.add(repository.save(new Remark("Remark_22", books.get(1).getId())));
+        remarks.add(repository.save(new Remark("Remark_31", books.get(2).getId())));
+        remarks.add(repository.save(new Remark("Remark_32", books.get(2).getId())));
+        remarks.add(repository.save(new Remark("Remark_33", books.get(2).getId())));
     }
 
     @ChangeSet(order = "005", id = "initBookRemark", author = "boytsov", runAlways = true)
@@ -72,7 +72,7 @@ public class InitTestMongoDBData {
             book.setRemarks(new ArrayList<Remark>());
         }
         books.get(0).getRemarks().add(remarks.get(0));
-        books.get(0).getRemarks().add(remarks.get(1));
+        books.get(1).getRemarks().add(remarks.get(1));
         books.get(1).getRemarks().add(remarks.get(2));
         books.get(2).getRemarks().add(remarks.get(3));
         books.get(2).getRemarks().add(remarks.get(4));
