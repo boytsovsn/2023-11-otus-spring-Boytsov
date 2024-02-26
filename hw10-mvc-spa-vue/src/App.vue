fffb38073ad92d264210e15cb8133abbdf5daa39
boytsovsn <caracupa@inbox.ru>
2024-02-26 10:50:21 +0700

    список книг работает

--- hw10-mvc-spa-vue/src/App.vue
+++ hw10-mvc-spa-vue/src/App.vue
@@ -1,30 +1,33 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+import apploader from './components/apploader.vue'
+//import books from './components/books.vue'
+import book from './components/book.vue'
+
+export default {
+  data() {
+    return {
+      title: 'Books',
+      title1: 'Book',
+      loading: false
+    }
+  },
+  methods: {
+
+  },
+
+  components: {book, apploader}
+}
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+    <apploader v-if="loading" />
+    <!-- <books v-else :BookTitle="title" /> -->
+    <book v-else :BookTitle="title1" />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+div{
+  display: flex;
+  min-height:100vh;
+}  
 </style>
--- hw10/src/test/java/ru/otus/hw/controllers/BookControllerTest.java
+++ hw10/src/test/java/ru/otus/hw/controllers/BookControllerTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.client.MockRestServiceServer;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -45,7 +47,7 @@ public class BookControllerTest {
         books.add(new BookDto("0515e04eeead46478298faa2", "Test title2", "65ce2a68d4437132eb18a432", "65ce2a68d4437132eb18a43e").toDomainObject());
         books.add(new BookDto("0515e04eeead46478298faa3", "Test title3", "65ce2a68d4437132eb18a433", "65ce2a68d4437132eb18a43f").toDomainObject());
         given(bookService.findAll()).willReturn(books);
-        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.get("/")
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.get("/api/books")
                         )
                 .andExpect(status().isOk())
                 .andDo(print())
@@ -56,7 +58,7 @@ public class BookControllerTest {
 
     @Test
     public void deleteBookDelete() throws Exception {
-        mockMvc.perform(delete("/book/0515e04eeead46478298faa1"))
+        mockMvc.perform(delete("/api/book/0515e04eeead46478298faa1").contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isMovedTemporarily()).andDo(print());
     }
 
