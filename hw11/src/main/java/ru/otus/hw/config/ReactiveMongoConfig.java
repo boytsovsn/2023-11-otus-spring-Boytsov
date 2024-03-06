d9297b6c41389dbde7d173ab1de894f61c5d067f
boytsovsn <caracupa@inbox.ru>
2024-03-07 02:32:53 +0700

    настройка монго

--- hw11/src/main/java/ru/otus/hw/config/ReactiveMongoConfig.java
+++ hw11/src/main/java/ru/otus/hw/config/ReactiveMongoConfig.java
@@ -4,8 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 import ru.otus.hw.repository.*;
 
-@EnableReactiveMongoRepositories(basePackageClasses = {PersonRepository.class, NotesRepository.class, AuthorRepository.class, GenreRepository.class, BookRepository.class, RemarkRepository.class})
-//@EnableMongock
+@EnableReactiveMongoRepositories(basePackages = {"ru.otus.hw.repository"})
 @Configuration
 public class ReactiveMongoConfig {
 
--- hw11/src/test/java/ru/otus/hw/changelogs/test/ReactiveMongoConfig.java
+++ hw11/src/test/java/ru/otus/hw/changelogs/test/ReactiveMongoConfig.java
@@ -1,13 +1,18 @@
 package ru.otus.hw.changelogs.test;
 
+import com.github.cloudyrock.spring.v5.EnableMongock;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 import ru.otus.hw.repository.PersonRepository;
 
 import static org.bson.codecs.configuration.CodecRegistries.fromProviders;
 import static org.bson.codecs.configuration.CodecRegistries.fromRegistries;
 
-@EnableReactiveMongoRepositories(basePackageClasses = {PersonRepository.class})
+@EnableReactiveMongoRepositories(basePackages = {"ru.otus.hw.repository"})
+//@EnableMongock
 @Configuration
 public class ReactiveMongoConfig {
 
