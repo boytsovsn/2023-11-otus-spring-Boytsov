edeb87082f5182d2ee738dcc1f757becbb048bd0
boytsovsn <caracupa@inbox.ru>
2024-03-26 17:38:46 +0700

    Попытка протестировать разрешения

--- hw13/src/main/java/ru/otus/hw/services/BookService.java
+++ hw13/src/main/java/ru/otus/hw/services/BookService.java
@@ -1,13 +1,18 @@
 package ru.otus.hw.services;
 
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PostFilter;
 import ru.otus.hw.models.entities.Book;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface BookService {
+
+    @PostAuthorize("hasPermission(returnObject, 'READ')")
     Optional<Book> findById(Long id);
 
+    @PostFilter("hasPermission(filterObject, 'READ')")
     List<Book> findAll();
 
     Book insert(String title, Long authorId, Long genreId);
